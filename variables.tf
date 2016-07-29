7fb528083464357545c348a8af4e749c360b3b93
pidster <pid@pidster.com>
2016-07-29 12:46:10 +0100

    More defaults in var file

--- variables.tf
+++ variables.tf
@@ -2,4 +2,14 @@ variable "access_key" {}
 variable "secret_key" {}
 variable "private_key_file" {}
 variable "aws_key_name" {}
+variable "aws_instance" {
+  default = "m4.xlarge"
+}
+variable "aws_region" {
+  default = "eu-west-1"
+}
+variable "aws_availability_zone" {
+  default = "eu-west-1b"
+}
+
 
